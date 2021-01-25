c40750755a88e7c04c8f856c6369a2a2fbe60696
PavelSuchkov <suchkovp@gmail.com>
2021-01-25 09:50:30 +0300

    commit

---
+++ react-1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (<header className='header'>
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjG-JZ1vOhYl5GhS0900uggNYgE6M7qWYNmw&usqp=CAU'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ react-1/src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://theinpaint.com/images/example-1-1.jpg" alt=""/>
+        </div>
+        <div>
+            AVA
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+                <div>
+                    post 3
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
